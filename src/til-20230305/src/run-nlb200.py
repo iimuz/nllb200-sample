95b1d9952c8bc44aad81f8cd6de275124279c3a5
iimuz <iz.primary.story+github@gmail.com>
2023-03-05 22:05:06 +0900

    feat: CPU実行コードを追加

---
+++ src/til-20230305/src/run-nlb200.py
@@ -0,0 +1,17 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "facebook/nllb-200-distilled-600M", cache_dir="data/external"
+)
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    "facebook/nllb-200-distilled-600M", cache_dir="data/external"
+)
+
+article = "I watched a lot of interesting animation last week."
+inputs = tokenizer(article, return_tensors="pt")
+
+translated_tokens = model.generate(
+    **inputs, forced_bos_token_id=tokenizer.lang_code_to_id["jpn_Jpan"], max_length=30
+)
+result = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+print(result)
